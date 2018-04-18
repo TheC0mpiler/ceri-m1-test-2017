7090be9b16e51ae6441598bbfbb8eb9a21c41bd0
TheC0mpiler <kudret.kustul.pro@gmail.com>
2018-04-18 18:47:41 +0200

    test GameStateProvider

--- src/test/java/fr/univavignon/rodeo/test/IGameStateProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/test/IGameStateProviderTest.java
@@ -1,6 +1,28 @@
 package fr.univavignon.rodeo.test;
 
-public class IGameStateProviderTest {
-	
+import static org.junit.Assert.assertEquals;
 
+import org.mockito.Mockito;
+
+import fr.univavignon.rodeo.api.IGameState;
+import fr.univavignon.rodeo.api.IGameStateProvider;
+
+public class IGameStateProviderTest {
+	private static IGameState gameState;
+	public static IGameStateProvider getTestInstanceStatic() {
+		IGameStateProvider gameStateProvider = Mockito.mock(IGameStateProvider.class);
+		gameState = IGameStateTest.getTestInstanceStatic();
+		Mockito.when(gameStateProvider.get("name")).thenReturn(gameState);
+		Mockito.when(gameStateProvider.get(null)).thenThrow(new IllegalArgumentException());
+		return gameStateProvider;
+	}
+
+	protected IGameStateProvider getTestInstance() {
+		return getTestInstanceStatic();
+	}
+	
+	public void testGet() {
+		IGameStateProvider test = getTestInstance();
+		assertEquals(test.get("name"),gameState);
+	}
 }
