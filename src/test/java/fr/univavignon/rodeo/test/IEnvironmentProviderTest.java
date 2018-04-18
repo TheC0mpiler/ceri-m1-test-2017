d0befb52244a7a5df3a512b5b4b271965143910e
TheC0mpiler <kudret.kustul.pro@gmail.com>
2018-04-18 17:56:28 +0200

    test environment provider

--- src/test/java/fr/univavignon/rodeo/test/IEnvironmentProviderTest.java
+++ src/test/java/fr/univavignon/rodeo/test/IEnvironmentProviderTest.java
@@ -1,5 +1,48 @@
 package fr.univavignon.rodeo.test;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import fr.univavignon.rodeo.api.IEnvironment;
+import fr.univavignon.rodeo.api.IEnvironmentProvider;
+
 public class IEnvironmentProviderTest {
+	
+	private static List<String> environmentsList;
+	private static IEnvironment environment;
+	
+	public static IEnvironmentProvider getTestInstanceStatic() {
+		
+		IEnvironmentProvider environmentProviderTest = Mockito.mock(IEnvironmentProvider.class);
+		
+		environmentsList = new LinkedList<String>();
+		environmentsList.add("list");
+		
+		Mockito.when(environmentProviderTest.getAvailableEnvironments()).thenReturn(environmentsList);
+		
+		environment = IEnvironmentTest.getTestInstanceStatic(4);
+		
+		Mockito.when(environmentProviderTest.getEnvironment("name")).thenReturn(environment);
+		
+		return environmentProviderTest;
+	}
+	
+	
+	@Test
+	public void testGetAvailableEnvironments() {
+		IEnvironmentProvider test = getTestInstanceStatic();
+		assertEquals(test.getAvailableEnvironments(),environmentsList);
+	}
+	
+	@Test
+	public void testgetEnvironment() {
+		IEnvironmentProvider test = getTestInstanceStatic();
+		assertEquals(test.getEnvironment("name"),environment);
+	}
 
 }
